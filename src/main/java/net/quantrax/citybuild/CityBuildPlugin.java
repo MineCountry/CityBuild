4eaa190f60308899004ae5e56824f93f097a3d66
Merry <svgr1808@outlook.de>
2024-02-15 22:43:52 +0100

    Implement toml file support

--- src/main/java/net/quantrax/citybuild/CityBuildPlugin.java
+++ src/main/java/net/quantrax/citybuild/CityBuildPlugin.java
@@ -1,14 +1,21 @@
 package net.quantrax.citybuild;
 
+import com.moandjiezana.toml.Toml;
 import net.quantrax.citybuild.backend.Environment;
 import net.quantrax.citybuild.backend.StaticSaduLoader;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CityBuildPlugin extends JavaPlugin {
 
+    private Toml toml;
+
     @Override
     public void onLoad() {
-        Environment.prepare();
+        Environment.prepareSystem();
+        Environment.copyToml(this);
+
         StaticSaduLoader.load();
+
+        toml = Environment.readConfig(this);
     }
 }
--- src/main/java/net/quantrax/citybuild/backend/Environment.java
+++ src/main/java/net/quantrax/citybuild/backend/Environment.java
@@ -1,11 +1,59 @@
 package net.quantrax.citybuild.backend;
 
+import com.moandjiezana.toml.Toml;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.codehaus.plexus.util.FileUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
 
 public final class Environment {
 
-    public static void prepare() {
+    public static void prepareSystem() {
         Dotenv env = Dotenv.configure().filename("credentials.env").load();
         env.entries().forEach(dotenvEntry -> System.setProperty(dotenvEntry.getKey(), dotenvEntry.getValue()));
+    }
+
+    public static @Nullable Toml readConfig(@NotNull Plugin plugin) {
+        File file = new File(plugin.getDataFolder() + "/CityBuild/config.toml");
+        return file.exists() ? new Toml().read(file) : null;
+    }
+
+    public static void copyToml(@NotNull JavaPlugin plugin) {
+        URL url = Environment.class.getClassLoader().getResource("toml/");
+
+        if (url == null) throw new IllegalStateException("No toml files were found");
+
+        List<File> files;
+        try (Stream<Path> fileStream = Files.walk(Paths.get(url.toURI()))) {
+            files = fileStream.filter(Files::isRegularFile)
+                    .map(Path::toFile)
+                    .toList();
+
+        } catch (IOException | URISyntaxException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        if (files.isEmpty()) return;
+
+        files.forEach(file -> {
+            try {
+                FileUtils.copyFile(file, new File(plugin.getDataFolder() + "/CityBuild/" + file.getName()));
+
+            } catch (IOException exception) {
+                throw new RuntimeException(exception);
+            }
+        });
     }
 }
