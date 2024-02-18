101c1909c1a32d2697e924fbb2aedf6b7244658e
Merry <svgr1808@outlook.de>
2024-02-18 21:26:24 +0100

    Mark template as required

--- src/main/java/net/quantrax/citybuild/utils/Log.java
+++ src/main/java/net/quantrax/citybuild/utils/Log.java
@@ -3,6 +3,7 @@ package net.quantrax.citybuild.utils;
 import com.google.errorprone.annotations.FormatMethod;
 import com.google.errorprone.annotations.FormatString;
 import org.bukkit.Bukkit;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.logging.Logger;
 
@@ -11,7 +12,7 @@ public final class Log {
     private static final Logger LOGGER = Bukkit.getLogger();
 
     @FormatMethod
-    public static void severe(@FormatString String template, Object... args) {
+    public static void severe(@FormatString @NotNull String template, Object... args) {
         LOGGER.severe(String.format(template, args));
     }
 }
