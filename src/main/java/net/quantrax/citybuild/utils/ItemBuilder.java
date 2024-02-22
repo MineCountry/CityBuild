c825af90a81667962128cb139774c4a7bea1606a
Merry <svgr1808@outlook.de>
2024-02-22 15:46:34 +0100

    Enhance builder functions

--- src/main/java/net/quantrax/citybuild/utils/ItemBuilder.java
+++ src/main/java/net/quantrax/citybuild/utils/ItemBuilder.java
@@ -1,23 +1,36 @@
 package net.quantrax.citybuild.utils;
 
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+
 public class ItemBuilder {
 
     private final ItemStack itemStack;
     private final ItemMeta itemMeta;
 
-    public ItemBuilder(Material material) {
+    public ItemBuilder(@NotNull Material material) {
         this.itemStack = new ItemStack(material, 1);
+        this.itemMeta = itemStack.getItemMeta();
+    }
+
+    public ItemBuilder(@NotNull ItemStack itemStack) {
+        this.itemStack = itemStack;
         this.itemMeta = itemStack.getItemMeta();
     }
 
     public ItemBuilder name(@NotNull TextComponent name) {
         itemMeta.displayName(name);
+        return this;
+    }
+
+    public ItemBuilder lore(Component... lines) {
+        itemMeta.lore(Arrays.asList(lines));
         return this;
     }
 
